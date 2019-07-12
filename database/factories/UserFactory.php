704e50132155c24d86bc67d4006e58816934b7b5
dawoodch <mdawoodch16@gmail.com>
2019-07-12 10:15:51 +0500

    part_11 implemented pagination and faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,4 +24,11 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+
+});
+
+
+$factory->afterCreating(User::class, function ($user, $faker){
+    $roles = \App\Role::where('name' , 'user')->get();
+    $user->roles()->sync($roles->pluck('id')->toArray());
 });
